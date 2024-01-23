dddc662463e97052db8cf22677561639cd2e5b2c
fatimashehab99 <fatima_shehab_sh@outlook.com>
2024-01-23 13:06:52 +0200

    downloading the json log file

--- data_engineering_test/src/main/java/org/example/Main.java
+++ data_engineering_test/src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Downloading the JSON logs link
+        try {
+            URL url = new URL("https://static.cognativex.com/test/logs.json");
+            InputStream inputStream = url.openStream();
+            //set the file destination where the json log will be saved
+            Path destination = Path.of("src/main/resources/logs.json");
+            ///copy the content of the json log file to the destination file
+            Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("File downloaded successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
