2e1f963917f4aca97f90c063cceffab564067d1e
fatimashehab99 <fatima_shehab_sh@outlook.com>
2024-01-25 01:52:37 +0200

    database connector

---
+++ data_engineering_test/src/main/java/org/example/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import javax.xml.transform.sax.SAXTransformerFactory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String Connector_URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private void testConnection() {
+        try (Connection connection = DriverManager.getConnection(Connector_URL, USERNAME, PASSWORD)) {
+            System.out.println("Connection successful");
+        } catch (SQLException e) {
+            System.err.println("Connection failed. Error message:  " + e.getMessage());
+        }
+    }
+
+
+
+
+}
--- data_engineering_test/src/main/java/org/example/Main.java
+++ data_engineering_test/src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.SQLException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -17,22 +18,22 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.example.DatabaseConnection.insertRecord;
 import static org.example.FileController.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws IOException, GeoIp2Exception {
+    public static void main(String[] args) throws IOException, GeoIp2Exception, SQLException {
 
-        //downloading the logs.json file if not exists
-        Path destination = Path.of("src/main/resources/logs.json");
-        if (!Files.exists(destination)) {
-            downloadingFile(destination);
-        }
-
-        ///reading logs.json file and extract the needed attributes
-        readingFile();
-
+//        //downloading the logs.json file if not exists
+//        Path destination = Path.of("src/main/resources/logs.json");
+//        if (!Files.exists(destination)) {
+//            downloadingFile(destination);
+//        }
+//
+//        ///reading logs.json file and extract the needed attributes
+//        readingFile();
 
     }
 }
